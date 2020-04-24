900d239c73ce4158626f1b51c30bedec48139e6e
Raj Shah <rnshah9@asu.edu>
2020-04-24 16:25:43 -0700

    Move sam file as well

--- scripts/simulate_illumina_reads.sh
+++ scripts/simulate_illumina_reads.sh
@@ -12,9 +12,13 @@ mkdir -p ./results/simulate_reads
 
 art_output="$(art_illumina -ss HS25 -i $1 -o $2 -l 150 -f 50 -p -m 500 -s 10 -sp -sam -na)"
 
+# grab fastq and sam output file names from output of art command
 fq1name="$(echo "$art_output" | grep "1st reads" | sed 's/.*reads:[[:space:]]\(.*\.fq\).*/\1/')"
 fq2name="$(echo "$art_output" | grep "2nd reads" | sed 's/.*reads:[[:space:]]\(.*\.fq\).*/\1/')"
+samname="$(echo "$art_output" | grep "\.sam" | sed 's/[[:space:]]\(.*\.sam\)/\1/')"
 
 mv "./$fq1name" "./results/simulate_reads/$fq1name"
 mv "./$fq2name" "./results/simulate_reads/$fq2name"
+mv "./$samname" "./results/simulate_reads/$samname"
 
+echo "Illumina read results can be found at ./results/simulate_reads/"
