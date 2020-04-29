8366bf1727d3e69994d1f6dc3f2bcef7d5b22bb3
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2020-04-28 22:27:38 -0700

    Update analysis.R for mtDNA

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,27 +1,42 @@
 
-# read mixed_n.txt
-
-# read haplogrep_n.result
 
 # x = variants from known profile
 # y = variants found by haplogrep
 
 # score = which(x=y)/x
 
-known_variants_list = read.csv("results/fasta/mixed_5.txt", skip = 2, header = FALSE, nrows = 1, stringsAsFactors = FALSE)
-known_variants = unlist(known_variants_list, use.names = FALSE)
+# Usage: Rscript --vanilla --quiet scripts/analysis.R $(find -name haplogrep_n.txt)
 
-haplogrep_result_dataframe = read.csv("results/haplogrep_5.txt", header = TRUE, sep = "\t")
-haplogrep_result = strsplit(haplogrep_result_dataframe$Found_Polys, " ")[[1]]
-
-count = 0
-
-for(i in known_variants) {
-  if(trimws(i) %in% haplogrep_result) {
-    count = count + 1
+main = function(haplogrep_files) {
+  known_files = gsub("haplogrep", "fasta/mixed", haplogrep_files)
+  score = vector()
+  
+  for(i in 1:length(haplogrep_files)) {
+    
+    known_variants_list = read.csv(known_files[i], skip = 2, header = FALSE, nrows = 1, stringsAsFactors = FALSE)
+    known_variants = unlist(known_variants_list, use.names = FALSE)
+    
+    haplogrep_result_dataframe = read.csv(haplogrep_files[i], header = TRUE, sep = "\t")
+    haplogrep_result = strsplit(haplogrep_result_dataframe$Found_Polys, " ")[[1]]
+    
+    count = 0
+    
+    for(j in known_variants) {
+      if(trimws(j) %in% haplogrep_result) {
+        count = count + 1
+      }
+    }
+    
+    score = rbind(score, count / length(known_variants))
+    
+    #writeLines()# results/fasta/mixed_n.txt 
   }
+  print(sum(score)/length(haplogrep_files))
+  cat(paste0("mtDNA identification score with n individuals and x runs is ",sum(score)/length(haplogrep_files)), file="analysis.txt", append = TRUE, sep = "\n")
+  
 }
 
-score = count / length(known_variants)
-
-writeLines()
+if(!interactive()) {
+  ARGS = commandArgs(trailing=TRUE)
+  main(ARGS)
+}
